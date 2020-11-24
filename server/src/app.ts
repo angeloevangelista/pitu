bf0eefecd8f086b91e0e57eebca83dc66fdb099b
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-11-24 16:49:12 -0300

    Enabling cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
